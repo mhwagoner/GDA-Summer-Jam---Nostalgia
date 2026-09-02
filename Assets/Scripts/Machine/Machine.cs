b32e04ec6121d1376af3904d496785d7e6fd8a01
FFreeze <quincymon@icloud.com>
2026-09-02 15:39:18 -0700

    Machine rotation

--- Assets/Scripts/Machine/Machine.cs
+++ Assets/Scripts/Machine/Machine.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Machine : MonoBehaviour
 {
@@ -6,10 +7,33 @@ public class Machine : MonoBehaviour
     private Object _protoObject;
 
     public Vector2 size;
+    public float rotationSpeed;
+
+    public InputActionAsset actionMap;
+    private InputAction _rotateL;
+    private InputAction _rotateR;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _rotateL = actionMap.FindAction("RotateL");
+        if (_rotateL == null)
+        {
+            Debug.Log("Could not find action \"RotateL\"");
+        }
+        else
+        {
+            _rotateL.Enable();
+        }
 
+        _rotateR = actionMap.FindAction("RotateR");
+        if (_rotateR == null)
+        {
+            Debug.Log("Could not find action \"RotateR\"");
+        }
+        else
+        {
+            _rotateR.Enable();
+        }
         size = GetComponent<SpriteRenderer>().bounds.size;
         /*
         const int iterations = 6;
@@ -31,7 +55,19 @@ public class Machine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_rotateL != null)
+        {
+            if (_rotateL.IsPressed())
+            {
+                transform.Rotate(0.0f, 0.0f, rotationSpeed * Time.deltaTime);
+            }
+        } 
+        if (_rotateR != null)
+        {
+            if (_rotateR.IsPressed())
+            {
+                transform.Rotate(0.0f, 0.0f, -rotationSpeed * Time.deltaTime);
+            }
+        }
     }
-
 }
