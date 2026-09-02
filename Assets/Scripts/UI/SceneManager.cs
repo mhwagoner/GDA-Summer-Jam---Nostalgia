eccfe53950081602b1cf970aa362cec85e160a9b
mhwagoner <michaelwagoner1971@gmail.com>
2026-09-02 15:39:19 -0700

    Bobbing Buttons

--- Assets/Scripts/UI/SceneManager.cs
+++ Assets/Scripts/UI/SceneManager.cs
@@ -8,4 +8,8 @@ public class SceneManager : MonoBehaviour
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
 }
