ec782069543b40984bf3911182bbd7a01c243f05
FFreeze <quincymon@icloud.com>
2026-09-01 20:40:18 -0700

    Prefabs consolidated, collision layers

--- Assets/Scripts/Objects/Object.cs
+++ Assets/Scripts/Objects/Object.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
 public class Object : MonoBehaviour
 {
     [SerializeField]
     private float _terminalVelocity;
 
+    [HideInInspector]
     public Rigidbody2D body;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
